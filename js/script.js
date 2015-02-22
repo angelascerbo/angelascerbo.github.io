8005d0de7a7a697f7f78fb790395695433a66895
angelascerbo <amscerbo@gmail.com>
2015-02-22 10:27:41 -0500

    fix to remove day

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function(){
   //mix it up
   $('#Container').mixItUp();  
   var isOpen = false;
-
+  var dayNum = $('#day-category li a').data('day');
   //logo click, active show all
   $('#logo').click(function(){
     $('#Container').mixItUp('filter', 'all');
@@ -41,6 +41,8 @@ $(function(){
 
     //remove item from pack list when star is unclicked
     if ($(this).hasClass('fa-plus-square-o') && $('#pack .button').hasClass('active')) {
+      $(this).closest('.mix').removeClass('day-' + dayNum);
+      console.log('will not remove day class');
       $(this).closest('.mix').fadeOut();
     }
 
@@ -53,15 +55,17 @@ $(function(){
 
     //check if no items have a star, slide up packing list button
     $('.mix').each (function() {
-      if($('.mix').hasClass("pack-item")){
+      if ($('.mix').hasClass("pack-item")){
         $('#pack').show();
       }
-      else if( $('#sort .button').hasClass('active') ||  $('#filter .button').hasClass('active')){
+      else if ($('#sort .button').hasClass('active') ||  $('#filter .button').hasClass('active')){
         $('#pack').slideUp();
-        $('.mix').removeClass('day-' + dayNum);
+        // removeDay(dayNum);
+
       } else {
         refresh();
       }
+
     //end of check each mix function
     });
 
@@ -77,13 +81,28 @@ $(function(){
       //end of modal click function
       });
 
-  //close modal without selecting day
-  $('#modal-close').click(function(){ 
-    $('.modal').modal('hide');
-  });
+      //close modal without selecting day
+      $('#modal-close').click(function(){ 
+        $('.modal').modal('hide');
+      });
 
     //end of assignDay  
     }
+
+    // function removeDay (dayNum) {
+    //   if (dayNum === 0) {
+    //       $('.mix').removeClass('day-' + dayNum);
+    //   }
+    // }
+  if ($(this).hasClass('fa-plus-square-o')) {
+    $(this).closest('.mix').removeClass('day-' + dayNum);
+    console.log('remove class is working on filter and sort');
+  }
+  // if ($(this).hasClass('fa-plus-square-o') && $('#pack .button').hasClass('active')) {
+  //   $(this).closest('.mix').removeClass('day-' + dayNum);
+  //   console.log('remove class?');
+  // }
+
   function refresh () {
     if ($('#pack').css("display","none")) {
       document.location.reload()
